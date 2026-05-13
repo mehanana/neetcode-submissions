62001ec3ebed4caa3b95b5c15d8fc5b5fc2d82b4
Mehana Nagarur <mehana.nagarur@gmail.com>
2026-05-13 18:00:23 -0400

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        /*
+        if n = 3
+        ((( )))
+        if we choose to add (, increase count by 1
+        if we choose to add ), decrease count by 1
+        count can't be less than 0 or more than n
+        */
+        List<String> res = new ArrayList<>();
+        dfs(res, n, 0, "");
+        return res;
+        
+    }
+    private void dfs(List<String> res, int n, int count, String curString) {
+        if (count < 0 || (curString.length() == n * 2 && count != 0)) {
+            // ) was added with no prev (  or  it's not well formed string
+            return;
+        } else if (curString.length() == n * 2 && count == 0) {
+            res.add(curString);
+            return;
+        }
+        String cur = curString;
+        dfs(res, n, count+1, curString + "(");
+        curString = cur;
+        dfs(res, n, count-1, curString + ")");
+    }
+
+}
