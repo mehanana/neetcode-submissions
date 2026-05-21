f4868294d93c3f4218ff1d810a09cb41a5780324
Mehana Nagarur <mehana.nagarur@gmail.com>
2026-05-21 18:12:46 -0400

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int rob(int[] nums) {
+        /*
+        [1 1 3 3]
+
+        start at index 0 -> can go to index 2, 3
+        at index 2 -> the end
+        at index 3 -> the end
+
+        start at index 1 -> can go to index 3
+        at index 3 -> the end
+
+        - start at 1st house & 2nd house
+        - skip the neighbor
+        */
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int one = nums[0];
+        int two = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            int temp = Math.max(nums[i] + one, two);
+            one = two;
+            two = temp;
+        }
+        return Math.max(one, two);
+        /*
+        int rob1 = 0, rob2 = 0;
+
+        for (int num : nums) {
+            int temp = Math.max(num + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+        return rob2;
+        */
+        
+    }
+}
