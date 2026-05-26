da25a8c3f1de7092cfec97c3a9c4a0a793608845
Mehana Nagarur <mehana.nagarur@gmail.com>
2026-05-26 00:33:56 -0400

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.java
@@ -0,0 +1,37 @@
+class Solution {
+    int[][] dp;
+    public int numDistinct(String s, String t) {
+        /*
+        - iterate through s until you find first letter of t
+        - then keep iterating until you find second, etc. until t is complete
+
+        - but you also want to keep searching in case there's more places where you find the second letter of t
+        */
+        int m = s.length(), n = t.length();
+        if (n > m) {
+            return 0;
+        }
+        dp = new int[s.length()+1][t.length()+1];
+        for (int i = 0; i <= m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return dfs(s, t, 0, 0);
+    }
+
+    private int dfs(String s, String t, int i, int j) {
+        if (j == t.length()) {
+            return 1;
+        }
+        if (i == s.length()) {
+            return 0;
+        }
+        if (dp[i][j] != -1) return dp[i][j];
+        
+        int res = dfs(s, t, i+1, j);
+        if (s.charAt(i) == t.charAt(j)) {
+            res += dfs(s, t, i+1, j+1);
+        }
+        dp[i][j] = res;
+        return res;
+    }
+}
