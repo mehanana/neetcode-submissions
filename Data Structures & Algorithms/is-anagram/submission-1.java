e1c6036bb6bbd34cb4c03a3989b4e6add84721ba
Mehana Nagarur <mehana.nagarur@gmail.com>
2026-06-01 14:05:50 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        /*
+        - go through s, store characters in hashmap and map the char to the number of times it appears
+        - go through t
+            - if char not in hashmap, return false
+            - if char in hashmap & count == 0, return false
+            - else, save count as count-1 and continue
+        */
+        Map<Character, Integer> chars = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            chars.put(c, chars.getOrDefault(c, 0) + 1); 
+        } 
+        for (char c : t.toCharArray()) {
+            if (!chars.containsKey(c)) {
+                return false;
+            }
+            if (chars.get(c) == 0) {
+                return false;
+            }
+            chars.put(c, chars.get(c)-1);
+        }
+        for (int i : chars.values()) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
