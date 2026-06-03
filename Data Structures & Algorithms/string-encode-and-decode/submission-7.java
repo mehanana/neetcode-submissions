e831500de5f81dd6f8c474e6d50f786b91bddcf1
Mehana Nagarur <mehana.nagarur@gmail.com>
2026-06-03 18:23:15 -0400

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.java
@@ -0,0 +1,73 @@
+class Solution {
+    /*
+    - length of string, followed by the string, followed by a period (to show that the string ended)
+    */
+
+    public String encode(List<String> strs) {
+        /*
+        length of string, period, string
+        */
+        String res = "";
+        for (String str : strs) {
+            res = res + str.length() + "." + str;
+        }
+        return res;
+
+        /*
+        String res = "";
+        for (String str : strs) {
+            res = res + str.length() + "." + str;
+        }
+        return res;
+        */
+
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        int index = 0;
+        while (index < str.length()) {
+            String curLength = "";
+            while (str.charAt(index) != '.') {
+                curLength = curLength+str.charAt(index);
+                index++;
+            }
+            index++;
+            int num = Integer.parseInt(curLength);
+            res.add(str.substring(index, index+num));
+            index = index+num;
+        }
+
+        return res;
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        List<String> res = new ArrayList<>();
+        int index = 0;
+        while (index < str.length()) {
+            String curLength = "";
+            while (str.charAt(index) != '.') {
+                curLength = curLength+str.charAt(index);
+                index++;
+            }
+            index++;
+            int length = Integer.parseInt(curLength);
+            String cur = str.substring(index, index+length);
+            index = index+length;
+            res.add(cur);
+        }
+        
+        return res;
+        */
+
+    }
+}
