94aac7cd7194c54069b10fbfefbe8c2ca6ad3402
Mehana Nagarur <mehana.nagarur@gmail.com>
2026-06-03 17:09:46 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+        int res = 0;
+        while (right < prices.length) {
+            if (prices[left] > prices[right]) {
+                left = right;
+            } else {
+                res = Math.max(res, prices[right] - prices[left]);
+            }
+
+            right++;
+
+
+        }
+        return res;
+    }
+}
