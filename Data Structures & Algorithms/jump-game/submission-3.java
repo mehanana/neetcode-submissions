31e4bbb8c04f4b6c0ac761dd24434fbbbe617674
Mehana Nagarur <mehana.nagarur@gmail.com>
2026-07-26 12:38:23 -0400

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int goal = n-1;
+
+        for (int i = n-2; i >= 0; i--) {
+            if (nums[i] + i >= goal) {
+                goal = i;
+            }
+        }
+        return goal == 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        int goal = nums.length-1;
+        for (int i = nums.length-2; i >= 0; i--) {
+            if (goal - i <= nums[i]) {
+                goal = i;
+            }
+        }
+        return goal == 0;
+        */
+        
+    }
+}
