cb410810b78168a2001b8aaa6aa75da3942a1bdb
Mehana Nagarur <mehana.nagarur@gmail.com>
2026-07-26 23:38:56 -0400

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.java
@@ -0,0 +1,106 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        /*
+        - sort cards
+        - put in hashmap (card val to numtimes it shows up)
+        - minval is the starting (and depending on how many times it occurs, that many times it starts)
+        */
+
+        if (hand.length % groupSize != 0) {
+            return false;
+        }
+
+        HashMap<Integer, Integer> freq = new HashMap();
+        Arrays.sort(hand);
+
+        for (int i : hand) {
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        }
+
+         
+
+        for (int i : hand) {
+            while (freq.getOrDefault(i, 0) > 0) {
+                if (freq.getOrDefault(i, 0) > 0) {
+                    freq.put(i, freq.get(i) - 1);
+                    for (int j = i + 1; j < i + groupSize; j++) {
+                        if (freq.getOrDefault(j, 0) > 0) {
+                            freq.put(j, freq.get(j) - 1);
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        1. sort hand
+        2. traverse through until you get to groupSize ( remove from list or smth )
+        3. then start from beginning of list, and do the same thing
+        - if at any point, you reach a value that's more than 1 away, return false
+        - can also check if hand % groupSize == 0 first
+
+        - build a frequency map of each value and traverse through that
+        */
+
+/*
+        if (hand.length % groupSize != 0) {
+            return false;
+        }
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int i : hand) {
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        }
+        List<Integer> keys = new ArrayList<>(freq.keySet());
+        Collections.sort(keys);
+        
+        for (int i : keys) {
+            while (freq.getOrDefault(i, 0) > 0) {
+                if (freq.getOrDefault(i, 0) > 0) {
+                    freq.put(i, freq.get(i) - 1);
+                    for (int j = i + 1; j < i + groupSize; j++) {
+                        if (freq.getOrDefault(j, 0) > 0) {
+                            freq.put(j, freq.get(j) - 1);
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+        */
+
+
+        
+    }
+}
